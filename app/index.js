ec390248c730282cdd040cd8b0948c557748ffb9
Simon Willcock <simon@willcock.com.au>
2016-08-31 22:28:05 +1000

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var mainStyles = require('./main.scss');
+
+ReactDOM.render(
+  <div className="container-fluid">
+    React WebPack Bourbon Neat
+  </div>, 
+  document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var NpmInstallPlugin = require('npm-install-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+
+var bourbon = require('node-bourbon').includePaths;
+var neat = require('node-neat').includePaths[1];
+var reset = require('node-reset-scss').includePath;
+
+const TARGET = process.env.npm_lifecycle_event;
+console.log("target event is " + TARGET);
+
+var common = {
+  cache: true,
+  debug: true,
+  entry: './app/index.js',
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    filename: 'index.js',
+    sourceMapFilename: '[file].map'
+  },
+  module: {
+    loaders: [{
+      test: /\.js[x]?$/,
+      loaders: ['babel-loader?presets[]=es2015&presets[]=react'],
+      exclude: /(node_modules|bower_components)/
+    }, {
+      test: /\.css$/,
+      loaders: ['style', 'css']
+    }, {
+      test: /\.scss$/,
+      loaders:  ['style', 'css', 'postcss', 'sass']
+    }, {
+      test: /\.less$/,
+      loaders: ['style', 'css', 'less']
+    }, {
+      test: /\.woff$/,
+      loader: "url-loader?limit=10000&mimetype=application/font-woff&name=[path][name].[ext]"
+    }, {
+      test: /\.woff2$/,
+      loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]"
+    }, {
+      test: /\.(eot|ttf|svg|gif|png)$/,
+      loader: "file-loader"
+    }]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ],
+  postcss: function() {
+    return [autoprefixer({
+      browsers: ['last 3 versions']
+    })];
+  },
+  sassLoader: {
+    includePaths: [reset, bourbon, neat]
+  }
+};
+
+if (TARGET === 'dev' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true
+    },
+    output: {
+      publicPath: 'http://localhost:8090/assets'
+    },
+    plugins: [
+      new NpmInstallPlugin({
+        save: true // --save
+      })
+    ]
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {
+    devtool: 'source-map',
+    output: {
+      path: './dist'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack-react',
+        template: 'index-template.ejs'
+      })
+    ]
+  });
+}
